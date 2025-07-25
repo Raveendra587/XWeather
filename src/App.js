495a8ceb99f08d3ad61b5941cc55f48a28ed314f
RuchithaMannam <ruchithamannam123@gmail.com>
2025-07-25 12:00:58 +0530

    updated App.js

--- src/App.js
+++ src/App.js
@@ -44,7 +44,7 @@ function App() {
         <button onClick={handleSearch}>Search</button>
       </div>
 
-      {loading && <p>Loading data…</p>}
+      {loading && <p>Loading data...</p>}
 
       {weatherData && (
         <div className="weather-cards">
